51cfece05b317b9b82556f698e0f3bd257e579cf
Jawwad <you@example.com>
2023-09-22 18:17:00 +0530

    22/09/2023

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalId = useRef(null);
+
+  
+
+  function handleStart(){
+
+    clearInterval(intervalId.current);
+
+    setStartTime(Date.now());
+
+    intervalId.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+
+  }
+
+  function handleStop(){
+    clearInterval(intervalId.current);
+  }
+
+  function handleReset(){
+    clearInterval(intervalId.current);
+    setNow(null);
+    setStartTime(null);
+  }
+
+  let timeLapsed = 0;
+
+  if(startTime != null && now!= null){
+    timeLapsed = (now - startTime)/1000;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Stopwatch</h1>
+    <div className='centered-div'>
+    <div className='timeLapsed'>{timeLapsed.toFixed(3)}</div>
+
+      <div className='button-container'>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+      </div>
+      
+    </div> 
+    </>
   );
 }
 
